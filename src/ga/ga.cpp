e24250da8ba8a02100d68d0000a0970ca0f7ec9e
Salomon Avila <salomon.avila2506@gmail.com>
2025-10-06 00:23:01 -0500

    database connection

--- src/ga/ga.cpp
+++ src/ga/ga.cpp
@@ -13,15 +13,82 @@ void obtainEnvData(std::vector<std::string> &v){
     }
 }
 
-std::string receiveGcResponse(zmq::socket_t& socket){
-    zmq::message_t response;
-    zmq::recv_result_t result =  socket.recv(response, zmq::recv_flags::none);
-    if(!result){
-        std::cerr<<"ERROR\n";
-        return 0;
+std::string renewBook(int code, int location, pqxx::connection &C){
+    int sede_real = location + 1;
+    
+    try {
+        pqxx::work W(C);
+
+        pqxx::result R = W.exec(
+            "SELECT id_estado, renovaciones "
+            "FROM estados e "
+            "JOIN libros l ON l.id_libro = e.id_libro "
+            "WHERE l.codigo = " + std::to_string(code) + 
+            " AND e.sede = " + std::to_string(sede_real) +
+            " AND e.tipo_operacion = 'prestamo' "
+            "ORDER BY e.fecha_operacion DESC "
+            "LIMIT 1"
+        );
+        
+        if (R.empty()) {
+            return "No se puede ejecutar: préstamo no encontrado.";
+        }
+        
+        int id_estado = R[0][0].as<int>();
+        int renovaciones = R[0][1].as<int>();
+        
+        if (renovaciones >= 2) {
+            return "No se puede renovar: se alcanzó el máximo de renovaciones.";
+        }
+  
+        W.exec(
+            "UPDATE estados "
+            "SET renovaciones = renovaciones + 1, "
+            "    fecha_devolucion_prevista = fecha_devolucion_prevista + INTERVAL '7 days' "
+            "WHERE id_estado = " + std::to_string(id_estado)
+        );
+        
+        W.commit();
+        return "Préstamo renovado exitosamente.";
+        
+    } catch (const std::exception &e) {
+        return std::string("Error: ") + e.what();
     }
-    std::string content((char *)response.data(), response.size());
-    return content;
+
+}
+
+std::string returnBook(int codigo, int sede, pqxx::connection &C) {
+    int sede_real = sede + 1;  
+    pqxx::work W(C);
+    
+    pqxx::result R = W.exec(
+        "SELECT e.id_estado, e.id_libro "
+        "FROM estados e "
+        "JOIN libros l ON l.id_libro = e.id_libro "
+        "WHERE l.codigo = " + std::to_string(codigo) + 
+        " AND e.sede = " + std::to_string(sede_real) + 
+        " AND e.tipo_operacion != 'devuelto' "
+        "ORDER BY e.fecha_operacion DESC "
+        "LIMIT 1;"
+    );
+    std::cout<<"Punto antes\n";
+    if (R.empty()) return "No se puede ejecutar: préstamo no encontrado.";
+    std::cout<<"Punto 1\n";
+    int id_estado = R[0]["id_estado"].as<int>();
+    int id_libro  = R[0]["id_libro"].as<int>();
+
+    W.exec(
+        "UPDATE estados SET tipo_operacion = 'devuelto' WHERE id_estado = " + std::to_string(id_estado) + ";"
+    );
+
+    std::string columna = (sede == 1) ? "ejemplares_sede1" : "ejemplares_sede2";
+    W.exec(
+        "UPDATE libros SET " + columna + " = " + columna + " + 1 WHERE id_libro = " + std::to_string(id_libro) + ";"
+    );
+
+    W.commit();
+    std::cout<<"Punto 2\n";
+    return "Préstamo devuelto exitosamente y ejemplares actualizados.";
 }
 
 int main(int argc, char *argv[]){
@@ -67,22 +134,27 @@ int main(int argc, char *argv[]){
         std::cout << " - Sede: " << int(req.location) << "\n";
 
         std::string reply;
-        socketOne.send(zmq::buffer(reply), zmq::send_flags::none);
         
         try{
             std::string conect = "dbname=root user=root password=root host=localhost port=5434";
             std::cout<<"Conexion es: "<<conect<<"\n";
             pqxx::connection C(conect);
-            
+            std::int8_t locat = req.location;
+            locat++;
+            std::cout<<"El codigo es: "<<int(req.requestType)<<"\n";
             switch(int(req.requestType)){
                 case 0:
                     std::cerr<<"TO BE IMPLEMENTED\n";
                     break;
                 case 1:
-                    //Funcion para renovacion
+                    std::cout<<"Opcion de renovar\n";
+                    reply = renewBook(req.code,req.location,C);
+                    socketOne.send(zmq::buffer(reply), zmq::send_flags::none);
                     break;
                 case 2:
-                    //Funcion para devolucion
+                    std::cout<<"Opcion de devolver\n";
+                    reply = returnBook(req.code,req.location,C);
+                    socketOne.send(zmq::buffer(reply), zmq::send_flags::none);
                     break;
             }
             
